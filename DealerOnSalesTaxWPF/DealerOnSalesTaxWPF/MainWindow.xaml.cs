92551d724a1b95b9ed3d842a89c66a8f9c7b2636
Zack Weber <weberz3@ferris.edu>
2020-01-22 19:11:05 -0500

    Decided early on to scrap the ConsoleApp idea, and go with WPF to give the user a simple user interface for item entry.

---
+++ DealerOnSalesTaxWPF/DealerOnSalesTaxWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DealerOnSalesTaxWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// DealerOn Coding Test
+        /// Problem #2: Sales Tax 
+        /// Author: Zack Weber
+        /// Date: 1/22/2020
+        /// Built using Visual Studio 2017
+        /// 
+        /// Overview: 
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
